3e784ac9021f97a288708360724b09d870bae783
Nathan Pabst <Nathan.Pabst@ey.com>
2019-06-09 14:55:10 -0500

    added *ngIf conditionals for isLoading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -38,6 +39,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>('https://angularhttpii.firebaseio.com/posts.json')
       .pipe(
@@ -52,6 +54,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }

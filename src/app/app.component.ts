0877f5d0e6643ba9c3d08549eb670c9b24db4c3b
Nathan Pabst <Nathan.Pabst@ey.com>
2019-06-09 13:50:29 -0500

    get request returning data to the console.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -31,5 +33,11 @@ export class AppComponent implements OnInit {
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://angularhttpii.firebaseio.com/posts.json').subscribe(posts => {
+      console.log(posts);
+    });
   }
 }

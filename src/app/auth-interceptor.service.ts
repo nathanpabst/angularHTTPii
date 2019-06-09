433e5818235d2e06c1592d27f5be78d3a1ba5f8c
Nathan Pabst <Nathan.Pabst@ey.com>
2019-06-09 18:17:15 -0500

    modifying request interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,6 +3,9 @@ import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http'
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('request is on its way');
-        return next.handle(req);
+        console.log(req.url);
+        // modifying a request...
+        const modifiedRequest = req.clone({ headers: req.headers.append('Auth', 'xyz') });
+        return next.handle(modifiedRequest);
     }
 }

56d517b7f498d326f7a12f5750576a5fc6f1d9ca
Nathan Pabst <Nathan.Pabst@ey.com>
2019-06-09 16:52:07 -0500

    example added for catchError.

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -36,6 +36,10 @@ export class PostsService {
                         }
                     }
                     return postsArray;
+                }),
+                catchError(errorRes => {
+                    // send to analytics server...
+                    return throwError(errorRes);
                 })
             );
     }
